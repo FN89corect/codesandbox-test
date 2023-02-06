59e1a742a78c928e5d6734d72ff86e9139415f15
FN89corect <124256587+FN89corect@users.noreply.github.com>
2023-02-06 19:05:17 +0900

    constについて

--- src/index.js
+++ src/index.js
@@ -30,7 +30,19 @@
 // val4.addres = "Kyoto";
 // console.log(val4);
 
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+const name = "健太";
+const age = 28;
+// 「私の名前は健太です。年齢は28です」
+
+/** 従来の方法*/
+const massage1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(massage1);
+
+// テンプレート
+const massage2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(massage2);
