3083e1153eb4b5c4d370659e977aaa9a0c1a0538
FN89corect <124256587+FN89corect@users.noreply.github.com>
2023-02-06 18:57:27 +0900

    const

---
+++ src/index.js
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数は再宣言可能";
+// console.log(val1);
+
+//
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // val3 = "const変数を上書き";
+// // console.log(val3);
+
+// onst val3 = "const変数を再宣言";
+// console.log(val3);
+
+// const val4 = {
+//   name: "健太",
+//   age: 28
+// };
+// val4.name = "Kenta";
+// val4.addres = "Kyoto";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
